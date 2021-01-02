41ba9365ffd4e499d215518b9cbe202fd2edcca3
ani5ruth <45996427+ani5ruth@users.noreply.github.com>
2021-01-02 12:55:08 +0530

    Update ProcessingChallengeOOPS.java

--- ProcessingChallengeOOPS.java
+++ ProcessingChallengeOOPS.java
@@ -1,7 +1,8 @@
 import processing.core.PApplet;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ProcessingChallengeOOPS extends PApplet {
 
@@ -9,42 +10,18 @@ public class ProcessingChallengeOOPS extends PApplet {
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
 
-    // custom class for moving circles with varying speed and height offset
-    private class MovingCircle {
-        private int xAxis;
-        private final int yAxisOffset;
-        private final int speed;
-
-        public MovingCircle(int yAxisOffset, int speed) {
-            this.xAxis = 0;
-            this.yAxisOffset = yAxisOffset;
-            this.speed = speed;
-        }
-
-        public void drawMovingCircle() {
-            drawCircle(xAxis, yAxisOffset * HEIGHT / 5);
-            this.xAxis += speed;
-        }
-    }
-
-    // list of given circles with their height and offset
-    int[][] circles = {{1, 1}, {2, 2}, {3, 3}, {4, 4}};
-
     public static void main(String[] args) {
         PApplet.main("ProcessingChallengeOOPS", args);
     }
+
+    float[][] circle = {{1.0f, 1.0f / 5}, {2.0f, 2.0f / 5}, {3.0f, 3.0f / 5}, {4.0f, 4.0f / 5}};
 
     List<MovingCircle> movingCircles;
 
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
-        movingCircles = new ArrayList<>();
-
-        //initialising circles
-        for (int[] circle : circles) {
-            movingCircles.add(new MovingCircle(circle[0], circle[1]));
-        }
+        movingCircles = Stream.of(circle).map(x -> new MovingCircle(x[0], x[1])).collect(Collectors.toList());
     }
 
     @Override
@@ -52,8 +29,24 @@ public class ProcessingChallengeOOPS extends PApplet {
         movingCircles.forEach(MovingCircle::drawMovingCircle);
     }
 
-    private void drawCircle(int xAxis, int yAxis) {
+    private void drawCircle(float xAxis, float yAxis) {
         ellipse(xAxis, yAxis, DIAMETER, DIAMETER);
     }
 
+    private class MovingCircle {
+        private float xAxis;
+        private final float yAxis;
+        private final float speed;
+
+        public MovingCircle(float speed, float yAxisOffset) {
+            this.xAxis = 0;
+            this.yAxis = yAxisOffset * HEIGHT;
+            this.speed = speed;
+        }
+
+        public void drawMovingCircle() {
+            drawCircle(xAxis, yAxis);
+            this.xAxis += speed;
+        }
+    }
 }
